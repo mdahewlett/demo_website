436b37538487a6e6e8a39a6d028484b1730bf6a5
mdah <mda.hewlett@gmail.com>
2025-01-26 17:04:30 -0800

    feat: update router for post endpoints

--- backend/backend/app/app/api/v1/api.py
+++ backend/backend/app/app/api/v1/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import (
-    weather,    
+    weather,
+    posts,    
 )
 
 api_router = APIRouter()
 api_router.include_router(weather.router, prefix="/weather", tags=["weather"])
+api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
